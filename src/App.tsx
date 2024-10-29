146af9a50a1bf89416c956a644368ff58ba4cd9a
Samael2228 <moneyecology@gmail.com>
2024-10-28 23:47:02 -0400

    creacion de componente home y cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> hola soy el commitico
-        </p>
-      </div>
-
+    <Home/>
 
     </>
   )
---
+++ src/components/Cards.tsx
@@ -0,0 +1,12 @@
+// Card.js
+import './Card.css'; // Agrega estilos específicos para la card
+
+function Card() {
+  return (
+  <div>
+    
+  </div>
+  );
+}
+
+export default Card;
